b3af465287db558625665746cb68bf178066a818
Fernando Souza <fernando.souza@liferay.com>
2016-07-14 18:50:42 -0300

    LPS-66783 The setValue method of the radio field is not a array.

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/main/resources/META-INF/resources/radio_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/main/resources/META-INF/resources/radio_field.js
@@ -68,7 +68,7 @@ AUI.add(
 
 						var radioToCheck = radiosNodeList.filter(
 							function(node) {
-								return node.val() === value[0];
+								return node.val() === value;
 							}
 						).item(0);
 
