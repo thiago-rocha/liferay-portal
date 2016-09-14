cb2b2751b09d0bcf63aa1f73bdc5b64bf29d8160
Fernando Souza <fernando.souza@liferay.com>
2016-09-14 17:26:17 -0300

    LPS-68076 SF

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/rule_builder.soy.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/templates/rule_builder.soy.js
@@ -10,7 +10,7 @@ if (typeof ddl == 'undefined') { var ddl = {}; }
 
 
 ddl.rule_builder = function(opt_data, opt_ignored) {
-  return '<div class="form-builder-rule-builder-container"><div class="form-builder-rule-builder-rules-list"></div><div class="form-builder-rule-builder-add-rule-container"><div class="btn-action-secondary btn-bottom-right form-builder-rule-builder-add-rule-button dropdown"><button class="btn btn-primary form-builder-rule-builder-add-rule-button-icon" type="button">' + soy.$$filterNoAutoescape(opt_data.plusIcon) + '</button></div></div></div>';
+  return '<div class="form-builder-rule-builder-container"><h1 class="form-builder-section-title text-default">Rules Builder</h1><ul class="ddl-form-body-content form-builder-rule-builder-rules-list tabular-list-group"></ul><div class="form-builder-rule-builder-add-rule-container"><div class="btn-action-secondary btn-bottom-right dropdown form-builder-rule-builder-add-rule-button"><button class="btn btn-primary form-builder-rule-builder-add-rule-button-icon" type="button">' + soy.$$filterNoAutoescape(opt_data.plusIcon) + '</button></div></div></div>';
 };
 if (goog.DEBUG) {
   ddl.rule_builder.soyTemplateName = 'ddl.rule_builder';
@@ -19,12 +19,12 @@ if (goog.DEBUG) {
 
 ddl.rule_list = function(opt_data, opt_ignored) {
   var output = '';
-  var ruleList158 = opt_data.rules;
-  var ruleListLen158 = ruleList158.length;
-  if (ruleListLen158 > 0) {
-    for (var ruleIndex158 = 0; ruleIndex158 < ruleListLen158; ruleIndex158++) {
-      var ruleData158 = ruleList158[ruleIndex158];
-      output += '<div class="card card-horizontal card-rule"><div class="card-row card-row-padded"><div class="card-col-content card-col-gutters"><p>' + soy.$$escapeHtml(ruleData158['logical-operator']) + '</p></div><div class="card-col-field"><div class="dropdown"><a class="dropdown-toggle icon-monospaced" data-toggle="dropdown" href="#1">' + soy.$$filterNoAutoescape(opt_data.kebab) + '</a><ul class="dropdown-menu dropdown-menu-right"><li class="rule-card-edit" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex158) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings.edit) + '</a></li><li class="rule-card-delete" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex158) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings['delete']) + '</a></li></ul></div></div></div></div>';
+  var ruleList169 = opt_data.rules;
+  var ruleListLen169 = ruleList169.length;
+  if (ruleListLen169 > 0) {
+    for (var ruleIndex169 = 0; ruleIndex169 < ruleListLen169; ruleIndex169++) {
+      var ruleData169 = ruleList169[ruleIndex169];
+      output += '<li class="list-group-item"><div class="list-group-item-field"><h4 class="form-builder-rule-builder-rules-list-type text-left">' + soy.$$escapeHtml(ruleData169['logical-operator']) + '</h4></div><div class="clamp-horizontal list-group-item-content"><p class="text-default">If ' + ddl.condition({content: ruleData169.conditions[0].operands[0].type + ' ' + ruleData169.conditions[0].operands[0].value}) + '<em> is ' + soy.$$escapeHtml(ruleData169.conditions[0].operator) + ' </em>,' + ddl.condition({content: ruleData169.conditions[0].operands[1].type + ' ' + ruleData169.conditions[0].operands[1].value}) + '<br />' + ddl.action({rule: ruleData169}) + '</p></div><div class="list-group-item-field"><div class="card-col-field"><div class="dropdown"><a class="dropdown-toggle icon-monospaced" data-toggle="dropdown" href="#1">' + soy.$$filterNoAutoescape(opt_data.kebab) + '</a><ul class="dropdown-menu dropdown-menu-right"><li class="rule-card-edit" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex169) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings.edit) + '</a></li><li class="rule-card-delete" data-card-id="' + soy.$$escapeHtmlAttribute(ruleIndex169) + '"><a href="javascript:;">' + soy.$$escapeHtml(opt_data.strings['delete']) + '</a></li></ul></div></div></div></li>';
     }
   } else {
     output += soy.$$escapeHtml(opt_data.strings.emptyListText);
