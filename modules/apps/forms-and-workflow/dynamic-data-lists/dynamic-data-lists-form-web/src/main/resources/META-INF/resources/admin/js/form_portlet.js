51d90a06701a90f0f7b83af643e7020a06f602da
Thiago Rocha <thiago.rocha@liferay.com>
2017-01-12 13:30:20 -0300

    LPS-69733 FE WIP (Refactoring the rule condition)

--- modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_portlet.js
+++ modules/apps/forms-and-workflow/dynamic-data-lists/dynamic-data-lists-form-web/src/main/resources/META-INF/resources/admin/js/form_portlet.js
@@ -758,6 +758,7 @@ AUI.add(
 						return new Liferay.DDL.FormBuilderRuleBuilder(
 							{
 								formBuilder: instance.get('formBuilder'),
+								functionsMetadata: instance.get('functionsMetadata'),
 								rules: instance.get('rules'),
 								visible: false
 							}
